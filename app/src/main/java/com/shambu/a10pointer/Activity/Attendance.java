c12e42fcfb8e53db5c74cd0ee80efdcde898a7bd
madhu2000u <madhu2000u@gmail.com>
2019-09-30 10:50:37 +0530

    All Necessary activity classes added

--- app/src/main/java/com/shambu/a10pointer/Activity/Attendance.java
+++ app/src/main/java/com/shambu/a10pointer/Activity/Attendance.java
@@ -1,14 +1,16 @@
-package com.shambu.a10pointer;
+package com.shambu.a10pointer.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.shambu.a10pointer.R;
+
+public class Attendance extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_time_table);
     }
 }
---
+++ app/src/main/java/com/shambu/a10pointer/Activity/Polls.java
@@ -0,0 +1,16 @@
+package com.shambu.a10pointer.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shambu.a10pointer.R;
+
+public class Polls extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_polls);
+    }
+}
