888c49d540c9f703de90f6ab5f94b09b51da688f
madhu2000u <madhu2000u@gmail.com>
2019-10-01 14:12:44 +0530

    Changed completely from activities to fragments

---
+++ app/src/main/java/com/shambu/a10pointer/Activity/MainActvity.java
@@ -0,0 +1,71 @@
+package com.shambu.a10pointer.Activity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.shambu.a10pointer.Fragments.attendance_fragment;
+import com.shambu.a10pointer.Fragments.cr_notific_fragment;
+import com.shambu.a10pointer.Fragments.grpchat_fragment;
+import com.shambu.a10pointer.Fragments.polls_fragment;
+import com.shambu.a10pointer.Fragments.timetable_fragment;
+import com.shambu.a10pointer.R;
+
+public class MainActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationListener);
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+
+            }
+        });
+    }
+
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selected_fragment=null;
+            switch (menuItem.getItemId()){
+                case R.id.cr_notif:
+                    selected_fragment=new cr_notific_fragment();
+                    break;
+                case R.id.timetable:
+                    selected_fragment=new timetable_fragment();
+                    break;
+                case R.id.attendance:
+                    selected_fragment=new attendance_fragment();
+                    break;
+                case R.id.polls:
+                    selected_fragment=new polls_fragment();
+                    break;
+                case R.id.groupchat:
+                    selected_fragment=new grpchat_fragment();
+                    break;
+
+
+            }
+
+            if (selected_fragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragments_container,selected_fragment).commit();
+            }
+
+            return true;
+        }
+    };
+}
---
+++ app/src/main/java/com/shambu/a10pointer/Fragments/polls_fragment.java
@@ -0,0 +1,23 @@
+package com.shambu.a10pointer.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.shambu.a10pointer.R;
+
+public class polls_fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_polls,container,false);
+
+
+    }
+}
